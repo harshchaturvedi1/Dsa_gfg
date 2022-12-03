56705eb9f60b4f14bf3ac0cc5e18539e5947a480
Harsh Chaturvedi <harsh.chaturvedi@olaelectric.com>
2022-12-03 11:15:48 +0530

    printing 1 to n without looping

--- School/4.1_to_n_without_loop.js
+++ School/4.1_to_n_without_loop.js
@@ -5,6 +5,10 @@
 
 /* 
 used recursion
+1.initialise 'temp' as empty and initialize a  variable 'i' from 1
+2.each time increment 'i' by '+1'
+3.add 'i' to temp till 'i' is less than 'N'
+4.end print temp
 */
 
 class Solution {
